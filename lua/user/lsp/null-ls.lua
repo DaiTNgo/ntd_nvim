573c022113d2a16555cd0b6bc17805ee90463be0
ntdai_linux <daingo7211@gmail.com>
2022-03-03 16:21:15 +0700

    1

---
+++ lua/user/lsp/null-ls.lua
@@ -0,0 +1,32 @@
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+
+if not null_ls_status_ok then
+	return
+end
+
+local formatting = null_ls.builtins.formatting
+-- local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier.with({
+			extra_filetypes = { "toml", "solidity" },
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
+		formatting.stylua,
+		-- formatting.black.with({ extra_args = { "--fast" } }),
+		-- diagnostics.eslint,
+		-- diagnostics.tsc,
+	},
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+		end
+	end,
+})
